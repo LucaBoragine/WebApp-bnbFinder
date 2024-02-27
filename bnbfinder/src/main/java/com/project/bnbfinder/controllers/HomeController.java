81912206ac01cfcd405bb83196bfcc4becaf9460
Luca Boragine <lucabora.lb@gmail.com>
2024-02-27 18:05:57 +0100

    aggiunta della home di css e js

--- bnbfinder/src/main/java/com/project/bnbfinder/controllers/HomeController.java
+++ bnbfinder/src/main/java/com/project/bnbfinder/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/")
 	public String home() {
-		return "home.html";		
+		return "home.jsp";		
 	}
 	
 }
