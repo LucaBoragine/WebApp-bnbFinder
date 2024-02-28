eba99096fedf56e9cb9bce605ce01c5ef49fd027
Eigherreight <161152721+Eigherreight@users.noreply.github.com>
2024-02-28 10:34:33 +0100

    Aggiornamenti
    BnbController,
    HomeController (ho inserito il link all'elenco città),
    Creato UtentiController
    Scritto DAOUtenti,
    Riportato il DAOStrutture di filippo

--- bnbfinder/src/main/java/com/project/bnbfinder/controllers/HomeController.java
+++ bnbfinder/src/main/java/com/project/bnbfinder/controllers/HomeController.java
@@ -1,13 +1,29 @@
 package com.project.bnbfinder.controllers;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	@GetMapping("/")
-	public String home() {
+	public String home(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if(session == null)
+			return "utenti/formlogin.html";
+		else
 		return "home.jsp";		
+	}
+	
+	@GetMapping("elencocitta")
+	public String elencocitta()
+	{
+		return "elencocitta.html";
 	}
 	
 }
